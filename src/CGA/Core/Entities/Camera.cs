29f148b2a2f37e347f87984cfa148ec23e54c23f
Igor Salivako <someaddress@yandex.ru>
2026-02-05 15:08:33 +0300

    feat: add Camera model

---
+++ src/CGA/Core/Entities/Camera.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Camera
+    {
+        public Vector3 EyePosition { get; set; } = Vector3.Zero;
+
+        public Vector3 TargetPosition { get; set; } = Vector3.Zero;
+
+        public Vector3 UpVector { get; set; } = Vector3.UnitY;
+
+        public float AspectRatio { get; set; } = 16f / 9f;
+
+        public float ZNear { get; set; } = 0.01f;
+
+        public float ZFar { get; set; } = 100f;
+
+        public float Radius { get; set; } = 5f;
+
+        public float Fov { get; set; } = MathF.PI / 2.0f;
+
+        private float Zeta { get; set; } = (float)Math.PI / (float)2.3;
+
+        private float Phi { get; set; } = (float)Math.PI / 2;
+
+        public void ChangeEyePosition()
+        {
+            EyePosition = new Vector3(
+                Radius * (float)Math.Cos(Phi) * (float)Math.Sin(Zeta),
+                Radius * (float)Math.Cos(Zeta),
+                Radius * (float)Math.Sin(Phi) * (float)Math.Sin(Zeta));
+        }
+    }
+}
