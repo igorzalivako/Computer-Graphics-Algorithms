925385ff71b327c423a57a38a4e374ca3b974132
Igor Salivako <someaddress@yandex.ru>
2026-02-05 15:11:49 +0300

    feat: add Scene model

---
+++ src/CGA/Core/Entities/Scene.cs
@@ -0,0 +1,34 @@
+﻿using Core.MatrixTransformations;
+
+namespace Core.Entities
+{
+    public class Scene
+    {
+        public ObjModel? ObjModel { get; set; }
+        
+        public Camera Camera { get; set; } = new();
+        
+        public int CanvasWidth { get; set; }
+        
+        public int CanvasHeight { get; set; }
+
+        public void TransformObject()
+        {
+            if (ObjModel is null)
+            {
+                throw new NullReferenceException("Object model is null");
+            }
+
+            var world = Transformations.CreateTransformationMatrix(ObjModel.Scale, ObjModel.Rotation, ObjModel.Position);
+
+            var view = Transformations.CreateViewMatrix(Camera.EyePosition, Camera.TargetPosition, Camera.UpVector);
+
+            var projection = Transformations.CreateProjectionMatrix(Camera.Fov, Camera.AspectRatio, Camera.ZNear, Camera.ZFar);
+
+            var viewport = Transformations.CreateViewportMatrix(CanvasWidth, CanvasHeight, 0.0f, 0.0f);
+
+            var transformMatrix = world * view * projection * viewport;
+            ObjModel.Transform(transformMatrix, Camera.ZNear, Camera.ZFar);
+        }
+    }
+}
